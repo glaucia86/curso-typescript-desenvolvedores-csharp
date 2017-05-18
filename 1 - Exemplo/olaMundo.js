cadefc759103fd348ef4ab66d09da42b9a038d5e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-05-18 20:47:33 -0300

    Aula 1 - Ola Mundo.

---
+++ 1 - Exemplo/olaMundo.js
@@ -0,0 +1,18 @@
+"use strict";
+/**
+ * 1) Executar: SHIFT + CTRL + B
+ * 2) No terminal executar: node (nome do arquivo)
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var OlaMundo = (function () {
+    function OlaMundo() {
+    }
+    OlaMundo.prototype.digaOlaTodos = function () {
+        console.log("Fala Coders!!!");
+    };
+    return OlaMundo;
+}());
+exports.OlaMundo = OlaMundo;
+var olaMundo = new OlaMundo();
+olaMundo.digaOlaTodos();
+//# sourceMappingURL=olaMundo.js.map
---
+++ 1 - Exemplo/olaMundo.ts
@@ -0,0 +1,13 @@
+/**
+ * 1) Executar: SHIFT + CTRL + B
+ * 2) No terminal executar: node (nome do arquivo)
+ */
+
+export class OlaMundo {
+    digaOlaTodos() {
+        console.log("Fala Coders!!!");
+    }
+}
+
+let olaMundo = new OlaMundo();
+olaMundo.digaOlaTodos();
