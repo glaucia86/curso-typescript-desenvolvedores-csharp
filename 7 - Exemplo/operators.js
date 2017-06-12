38fd3a5ab66e8eddd99c898a929ff4124d0e5757
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2017-06-11 22:47:55 -0300

    Demo sobre Operadores Aritméticos em TypeScript.

---
+++ 7 - Exemplo/operators.js
@@ -0,0 +1,70 @@
+/**
+ * Arquivo: operators.ts
+ * Author: Glaucia Lemos
+ * Description: Nesse exemplo estamos vendo exemplo
+ * do uso de operações aritméticas em TypeScript
+ * Data: 11/06/2017
+ * Comando para executar:
+ *  1) Executar o comando: Shift + Ctrl + B (Windows)
+ *  2) No terminal executar: Node (nome do arquivo)
+ *
+ * LINK: http://www.w3ii.com/pt/typescript/typescript_operators.html
+ */
+// Operador - Soma:
+function sumOperator(num1, num2) {
+    resultado = num1 + num2;
+    console.log("A soma é......: " + resultado);
+}
+sumOperator(10, 2);
+// Operador - Subtração:
+function subOperator(num1, num2) {
+    resultado = num1 - num2;
+    console.log("A subtração é......: " + resultado);
+}
+subOperator(10, 2);
+// Operador - Multiplicação:
+function multiOperator(num1, num2) {
+    resultado = num1 * num2;
+    console.log("A multiplicação é......: " + resultado);
+}
+multiOperator(10, 2);
+// Operador - Divisão:
+function divideOperator(num1, num2) {
+    resultado = num1 / num2;
+    console.log("A divisão é......: " + resultado);
+}
+divideOperator(10, 2);
+// Operador - Módulo:
+function modOperator(num1, num2) {
+    resultado = num1 % num2;
+    console.log("O módulo é......: " + resultado);
+}
+modOperator(10, 2);
+// Operador - Incremento:
+var num1 = 17;
+num1++;
+console.log("O incremento é.....: " + num1);
+// Operador - Decremento:
+var num2 = 15;
+num2--;
+console.log("O decremento é......: " + num2);
+// Realizar exercícios com:
+/**
+ *
+ * 1) Operadores Relacionais:
+ *
+ * http://www.w3ii.com/pt/typescript/typescript_relational_operators_examples.html
+ *
+ * 2) Operadores de Atribuição:
+ *
+ * http://www.w3ii.com/pt/typescript/typescript_assignment_operators_examples.html
+ *
+ * 3) Operador condicional (?)
+ *
+ * http://www.w3ii.com/pt/typescript/typescript_operators.html
+ *
+ * 4) Operador typeof
+ *
+ * http://www.w3ii.com/pt/typescript/typescript_operators.html
+ */ 
+//# sourceMappingURL=operators.js.map
